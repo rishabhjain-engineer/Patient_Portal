1d3c75e040f0370e7e8dcf75c1b9fa945f23795b
Rishabh <rishabhjain.engineer@gmail.com>
2017-02-15 15:10:14 +0530

    local :

--- app/src/main/java/config/StaticHolder.java
+++ app/src/main/java/config/StaticHolder.java
@@ -15,14 +15,14 @@ public class StaticHolder {
 
     /*Server Locations */
 
-    public final String BASE_URL = "https://api.healthscion.com/WebServices/LabService.asmx/"; //LIVE //TODO commented by ayaz
+    //public final String BASE_URL = "https://api.healthscion.com/WebServices/LabService.asmx/"; //LIVE //TODO commented by ayaz
    // public final String BASE_URL = "http://ec2-54-169-123-240.ap-southeast-1.compute.amazonaws.com/WebServices/LabService.asmx/"; // testing
-    //public final String BASE_URL = "http://192.168.1.11/WebServices/LabService.asmx/"; //LOCAL //TODO opened by ayaz
+    public final String BASE_URL = "http://192.168.1.11/WebServices/LabService.asmx/"; //LOCAL //TODO opened by ayaz
     //  public final String LIVELOGIN_URL = "https://l141702.cloudchowk.com/";     //live
     // public  final String LIVELOGIN_URL = "https://d141702.cloudchowk.com/";//demo
     // public final String LIVELOGIN_URL = "http://192.168.1.56:8085/";// local
-    //public final String BASE_URL1 = "http://192.168.1.11/";//local //TODO opened by ayaz
-    public final String BASE_URL1 = "https://api.healthscion.com/"; //TODO commented by ayaz
+    public final String BASE_URL1 = "http://192.168.1.11/";//local //TODO opened by ayaz
+    //public final String BASE_URL1 = "https://api.healthscion.com/"; //TODO commented by ayaz
     // public final String BASE_URL1 = "http://ec2-54-169-123-240.ap-southeast-1.compute.amazonaws.com/";// testing
 
     //APIs URLs
@@ -219,9 +219,9 @@ public class StaticHolder {
                 url = BASE_URL1 + "WebServices/CredentialsService.asmx/UpdateImage";
                 break;
             case GetpatienttestReportAndroid:
-                 url = "https://api.healthscion.com/WebServices/HTMLReports.asmx/GetpatienttestReportHTMLAndroid"; //TODO ayaz uncomment it on live
+                // url = "https://api.healthscion.com/WebServices/HTMLReports.asmx/GetpatienttestReportHTMLAndroid"; //TODO ayaz uncomment it on live
                 //url = "https://api.healthscion.com/WebServices/HTMLReports.asmx/GetpatienttestReportHTMLAndroid";
-                //url = "http://192.168.1.11/WebServices/HTMLReports.asmx/GetpatienttestReportHTMLAndroid"; // Local //TODO ayaz uncomment it on local
+                url = "http://192.168.1.11/WebServices/HTMLReports.asmx/GetpatienttestReportHTMLAndroid"; // Local //TODO ayaz uncomment it on local
                 //http://192.168.1.202:86/WebServices/HTMLReports.asmx
                 break;
             case GetUserDetailsFromContactNoMobileService:
