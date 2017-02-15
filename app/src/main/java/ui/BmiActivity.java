fda1d62651a98f8e43a3e1c870ebdcdce37bfc9d
ayazAndroid <ayazhealthscion.com>
2017-02-15 15:49:25 +0530

    Fixed issue

--- app/src/main/java/ui/BmiActivity.java
+++ app/src/main/java/ui/BmiActivity.java
@@ -1,10 +1,17 @@
 package ui;
 
 import android.app.Activity;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
 
 /**
  * Created by android1 on 15/2/17.
  */
 
 public class BmiActivity extends BaseActivity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setupActionBar();
+    }
 }
