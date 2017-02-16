9001c5a153e00019599acb82599b0fc0badff375
Rishabh <rishabhjain.engineer@gmail.com>
2017-02-16 11:04:00 +0530

    bp2linechart

--- app/src/main/java/ui/BmiActivity.java
+++ app/src/main/java/ui/BmiActivity.java
@@ -103,7 +103,7 @@ public class BmiActivity extends BaseActivity {
         settings.setSupportZoom(true);
         settings.setUserAgentString("Mozilla/5.0 (Linux; Android 4.0.4; Galaxy Nexus Build/IMM76B) AppleWebKit/535.19 (KHTML, like Gecko) Chrome/18.0.1025.133 Mobile Safari/535.19");
         weight_graphView.setInitialScale(1);
-   //     weight_graphView.addJavascriptInterface(new BpActivity.MyJavaScriptInterface(), "Interface");
+           weight_graphView.addJavascriptInterface(new MyJavaScriptInterface(), "Interface");
 
         queue = Volley.newRequestQueue(this);
         if (!NetworkChangeListener.getNetworkStatus().isConnected()) {
