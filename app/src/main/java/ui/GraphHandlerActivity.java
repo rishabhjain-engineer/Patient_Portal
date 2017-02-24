60f6595503e1f1413ba73918f1c290d8462dbe01
ayazAndroid <ayazhealthscion.com>
2017-02-24 11:34:03 +0530

    Fixed Issue

--- app/src/main/java/ui/GraphHandlerActivity.java
+++ app/src/main/java/ui/GraphHandlerActivity.java
@@ -46,6 +46,7 @@ public class GraphHandlerActivity extends BaseActivity {
         JSONArray jsonArray = new JSONArray();
         while (!calendar.after(cal2)) {
             Date dateToConvert = calendar.getTime();
+            Log.i("Daily", "Date: " + dateToConvert);
             long epoch = dateToConvert.getTime();
             JSONArray innerJsonArray = new JSONArray();
             innerJsonArray.put(epoch);
@@ -94,6 +95,7 @@ public class GraphHandlerActivity extends BaseActivity {
             Date date = null;
             try {
                 date = formater.parse(dateInString);
+                Log.i("Weekly", "Weekly: " + date);
             } catch (ParseException e) {
                 e.printStackTrace();
             }
@@ -109,10 +111,18 @@ public class GraphHandlerActivity extends BaseActivity {
 
     protected JSONArray getJsonForMonthly(String date1, String date2) {
         String dateArray[] = date1.split("/");
-        String month = dateArray[1];
-        int mont = Integer.parseInt(month);
-        mont = mont + 1;
-        date1 = "01/" + mont + "/" + dateArray[2];
+        String monthInString = dateArray[1];
+        String yearInString = dateArray[2];
+        int monthInInt = Integer.parseInt(monthInString);
+        int yearInInt = Integer.parseInt(yearInString);
+        //int mont = Integer.parseInt(month);
+        if(monthInInt == 12){
+            monthInInt =  1;
+            yearInInt = yearInInt + 1;
+        }else{
+            monthInInt = monthInInt + 1;
+        }
+        date1 = "01/" + monthInInt + "/" + yearInInt;
         SimpleDateFormat formater = new SimpleDateFormat("dd/MM/yyyy");
         Calendar beginCalendar = Calendar.getInstance();
         Calendar finishCalendar = Calendar.getInstance();
@@ -126,10 +136,11 @@ public class GraphHandlerActivity extends BaseActivity {
         JSONArray jsonArray = new JSONArray();
         while (beginCalendar.before(finishCalendar)) {
             String dateInString = formater.format(beginCalendar.getTime()).toUpperCase();
-            Log.i("ayaz", "Date: " + date);
+            Log.i("ayaz", "Date: " + dateInString);
             Date date = null;
             try {
                 date = formater.parse(dateInString);
+                Log.i("monthly", "monthly: " + date);
             } catch (ParseException e) {
                 e.printStackTrace();
             }
@@ -181,6 +192,7 @@ public class GraphHandlerActivity extends BaseActivity {
             Date date = null;
             try {
                 date = formater.parse(dateInString);
+                Log.i("quaterly", "quaterly: " + date);
             } catch (ParseException e) {
                 e.printStackTrace();
             }
@@ -228,6 +240,7 @@ public class GraphHandlerActivity extends BaseActivity {
             Date date = null;
             try {
                 date = formater.parse(dateInString);
+                Log.i("semi", "semi: " + date);
             } catch (ParseException e) {
                 e.printStackTrace();
             }
@@ -267,6 +280,7 @@ public class GraphHandlerActivity extends BaseActivity {
             Date date = null;
             try {
                 date = formater.parse(dateInString);
+                Log.i("yearly", "yearly: " + date);
             } catch (ParseException e) {
                 e.printStackTrace();
             }
