a9338b159ee7204dabcff63dd8dc39169325e98d
ayazAndroid <ayazhealthscion.com>
2017-03-17 09:38:22 +0530

    Sorted in Reverse Order

--- app/src/main/java/com/hs/userportal/GraphDetailValueAndDate.java
+++ app/src/main/java/com/hs/userportal/GraphDetailValueAndDate.java
@@ -1,10 +1,12 @@
 package com.hs.userportal;
 
+import java.util.Comparator;
+
 /**
  * Created by android1 on 25/2/17.
  */
 
-public class GraphDetailValueAndDate {
+public class GraphDetailValueAndDate implements Comparable<GraphDetailValueAndDate>{
     private String value;
     private String date;
     private String caseCode;
@@ -31,5 +33,10 @@ public class GraphDetailValueAndDate {
 
     public void setCaseCode(String caseCode) {
         this.caseCode = caseCode;
+    }
+
+    @Override
+    public int compareTo(GraphDetailValueAndDate o) {
+        return this.date.compareTo(o.getDate());
     }
 }
