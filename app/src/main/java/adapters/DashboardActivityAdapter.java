2cbf8ab1620b964b5e8d0d7138c796fa6d9a5752
ayazAndroid <ayazhealthscion.com>
2017-03-30 12:46:43 +0530

    Added Dashboard items

--- app/src/main/java/adapters/DashboardActivityAdapter.java
+++ app/src/main/java/adapters/DashboardActivityAdapter.java
@@ -1,55 +1,89 @@
 package adapters;
 
+import android.app.Activity;
 import android.content.Context;
+import android.content.Intent;
+import android.graphics.drawable.Drawable;
+import android.text.TextUtils;
+import android.text.style.DrawableMarginSpan;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.BaseAdapter;
 import android.widget.ImageView;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 
 import org.askerov.dynamicgrid.BaseDynamicGridAdapter;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import com.hs.userportal.R;
+import com.hs.userportal.VaccineDetails;
+
+import ui.DashBoardActivity;
 
 /**
  * Created by ayaz on 29/3/17.
  */
 
-public class DashboardActivityAdapter extends BaseDynamicGridAdapter {
-    public DashboardActivityAdapter(Context context, List<String> items, int columnCount) {
-        super(context, items, columnCount);
+public class DashboardActivityAdapter extends BaseAdapter {
+    private Activity mActivity;
+    private List<String> mList = new ArrayList<>();
+    private List<Integer> mImageList = new ArrayList<>();
+
+    public DashboardActivityAdapter(DashBoardActivity dashBoardActivity) {
+        mActivity = dashBoardActivity;
+
+        mList.add("Vital 1");
+        mList.add("Vital 2");
+        mList.add("Discounts");
+        mList.add("Coach");
+        mList.add("Alerts");
+
+        mImageList.add(R.drawable.vital1_144);
+        mImageList.add(R.drawable.vital2_144);
+        mImageList.add(R.drawable.discounts_144);
+        mImageList.add(R.drawable.coach_144);
+        mImageList.add(R.drawable.alerts_144);
+    }
+
+
+    @Override
+    public int getCount() {
+        return mList.size();
+    }
+
+    @Override
+    public Object getItem(int position) {
+        return mList.get(position);
+    }
+
+    @Override
+    public long getItemId(int position) {
+        return 0;
+    }
+
+    private class ViewHolder {
+        TextView name;
+        ImageView image;
     }
 
     @Override
     public View getView(int position, View convertView, ViewGroup parent) {
-
-        CountryListViewHolder holder;
+        ViewHolder holder;
         if (convertView == null) {
-
-            convertView = LayoutInflater.from(getContext()).inflate(R.layout.dashboard_grid_view, null);
-            holder = new CountryListViewHolder(convertView);
+            convertView = LayoutInflater.from(mActivity).inflate(R.layout.dashboard_grid_view, parent, false);
+            holder = new ViewHolder();
+            holder.name = (TextView) convertView.findViewById(R.id.image_text_view);
+            holder.image = (ImageView) convertView.findViewById(R.id.image_view);
             convertView.setTag(holder);
-
         } else {
-
-            holder = (CountryListViewHolder) convertView.getTag();
+            holder = (ViewHolder) convertView.getTag();
         }
-
-        holder.titleText.setText(getItem(position).toString());
-        holder.image.setImageResource(R.drawable.ic_launcher);
+        holder.name.setText(mList.get(position));
+        holder.image.setImageResource(mImageList.get(position));
         return convertView;
-    }
-
-    private class CountryListViewHolder {
-        private TextView titleText;
-        private ImageView image;
-
-        private CountryListViewHolder(View view) {
-            titleText = (TextView) view.findViewById(R.id.title);
-            image = (ImageView) view.findViewById(R.id.image);
-        }
-
     }
 }
--- app/src/main/java/ui/DashBoardActivity.java
+++ app/src/main/java/ui/DashBoardActivity.java
@@ -5,16 +5,29 @@ import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.AdapterView;
+import android.widget.GridLayout;
+import android.widget.GridView;
 import android.widget.Toast;
 
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.Volley;
 import com.hs.userportal.R;
 
 import org.askerov.dynamicgrid.DynamicGridView;
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
 
+import java.text.DecimalFormat;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 
 import adapters.DashboardActivityAdapter;
+import config.StaticHolder;
 
 /**
  * Created by ayaz on 29/3/17.
@@ -24,6 +37,9 @@ public class DashBoardActivity extends Activity {
     private DynamicGridView mDaDynamicGridView;
     private List<String> mList = new ArrayList<>();
     private String privatery_id;
+    private static RequestQueue request;
+    private ArrayList<HashMap<String, String>> family_object;
+    private GridView mGridView;
 
     public static String image_parse;
     public static String emailid;
@@ -34,5 +50,94 @@ public class DashBoardActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_dashboard);
+        mGridView = (GridView) findViewById(R.id.grid_view);
+        DashboardActivityAdapter dashboardActivityAdapter = new DashboardActivityAdapter(this);
+        mGridView.setAdapter(dashboardActivityAdapter);
+        //findFamily();
+    }
+
+    private void findFamily() {
+        request = Volley.newRequestQueue(this);
+        StaticHolder static_holder = new StaticHolder(this, StaticHolder.Services_static.GetMember);
+        String url = static_holder.request_Url();
+        JSONObject data = new JSONObject();
+        try {
+            data.put("patientId", id);
+        } catch (JSONException je) {
+            je.printStackTrace();
+        }
+        Log.i("GetMember", "url: "+url);
+        Log.i("GetMember", "data to Send: "+data);
+        JsonObjectRequest family = new JsonObjectRequest(com.android.volley.Request.Method.POST, url, data, new Response.Listener<JSONObject>() {
+            @Override
+            public void onResponse(JSONObject response) {
+                try {
+                    Log.i("GetMember", "Received Data: "+response);
+                    String data = response.getString("d");
+                    JSONObject j = new JSONObject(data);
+                    JSONArray family_arr = j.getJSONArray("Table");
+                    family_object = new ArrayList<HashMap<String, String>>();
+                    if (family_arr.length() == 0) {
+                    } else {
+                        HashMap<String, String> hmap;
+                        for (int i = 0; i < family_arr.length(); i++) {
+                            JSONObject json_obj = family_arr.getJSONObject(i);
+                            hmap = new HashMap<String, String>();
+                            hmap.put("FirstName", json_obj.getString("FirstName"));
+                            hmap.put("PatientCode", json_obj.getString("PatientCode"));
+                            hmap.put("LastName", json_obj.getString("LastName"));
+                            hmap.put("ContactNo", json_obj.getString("ContactNo"));
+                            hmap.put("Sex", json_obj.getString("Sex"));
+                            if (json_obj.has("PatientId")) {
+                                hmap.put("PatientId", json_obj.getString("PatientId"));
+                            } else {
+                                hmap.put("PatientId", "");
+                            }
+                            hmap.put("FamilyMemberId", json_obj.getString("FamilyMemberId"));
+                            hmap.put("FamilyHeadId", json_obj.getString("FamilyHeadId"));
+                            if (json_obj.has("Result")) {
+                                hmap.put("Result", json_obj.getString("Result"));
+                            } else {
+                                hmap.put("Result", "");
+                            }
+                            hmap.put("Processing", json_obj.getString("Processing"));
+                            if (json_obj.has("Result")) {
+                                hmap.put("TestName", json_obj.getString("TestName"));
+                            } else {
+                                hmap.put("TestName", "");
+                            }
+                            if (json_obj.has("Result")) {
+                                hmap.put("DateOfReport", json_obj.getString("DateOfReport"));
+                            } else {
+                                hmap.put("DateOfReport", "");
+                            }
+                            hmap.put("Image", json_obj.getString("Image"));
+                            hmap.put("Age", json_obj.getString("Age"));
+                            hmap.put("RelationName", json_obj.getString("RelationName"));
+                            hmap.put("HM", json_obj.getString("HM"));
+                            hmap.put("IsApproved", json_obj.getString("IsApproved"));
+                            hmap.put("IsMemberRemoved", json_obj.getString("IsMemberRemoved"));
+                            if (json_obj.getString("IsApproved").equals("true")) {
+                                family_object.add(hmap);
+                            }
+                        }
+                        int s = family_object.size();
+                        String size = new DecimalFormat("00").format(s);
+                        //members.setText(size);
+                    }
+                } catch (JSONException je) {
+                    je.printStackTrace();
+                    onBackPressed();
+                    Toast.makeText(getBaseContext(), "Some error occurred.Please try again later.", Toast.LENGTH_SHORT).show();
+                }
+            }
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                onBackPressed();
+                Toast.makeText(getBaseContext(), "Some error occurred.Please try again later.", Toast.LENGTH_SHORT).show();
+            }
+        });
+        request.add(family);
     }
 }
