369bde7a97a9271f20f0a292d6e3ccb43117a002
ayazAndroid <ayazhealthscion.com>
2017-06-06 14:09:16 +0530

    Upload files

--- app/src/main/java/ui/SymptomsActivity.java
+++ app/src/main/java/ui/SymptomsActivity.java
@@ -1,26 +1,269 @@
 package ui;
 
+import android.Manifest;
+import android.app.Activity;
+import android.app.AlertDialog;
+import android.app.Dialog;
+import android.app.ProgressDialog;
+import android.content.ClipData;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.content.pm.PackageManager;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.drawable.ColorDrawable;
+import android.net.Uri;
+import android.os.Build;
 import android.os.Bundle;
+import android.os.Environment;
+import android.provider.MediaStore;
+import android.provider.Settings;
+import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.FileProvider;
+import android.view.View;
+import android.view.Window;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
 
+import com.applozic.audiovideo.activity.AudioCallActivityV2;
+import com.applozic.audiovideo.activity.VideoActivity;
+import com.applozic.mobicomkit.ApplozicClient;
+import com.applozic.mobicomkit.uiwidgets.conversation.ConversationUIService;
+import com.applozic.mobicomkit.uiwidgets.conversation.activity.ConversationActivity;
+import com.hs.userportal.Directory;
 import com.hs.userportal.R;
+import com.hs.userportal.UploadService;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Date;
+
+import fragment.RepositoryFreshFragment;
+import networkmngr.NetworkChangeListener;
+import utils.RepositoryUtils;
 
 /**
  * Created by ayaz on 6/6/17.
  */
 
 public class SymptomsActivity extends BaseActivity {
+    private Activity mActivity;
+    String[] permissionsRequired = new String[]{Manifest.permission.CAMERA, Manifest.permission.WRITE_EXTERNAL_STORAGE};
+    private static final int PERMISSION_CALLBACK_CONSTANT = 100;
+    private static final int REQUEST_PERMISSION_SETTING = 101;
+    private static final int MY_PERMISSIONS_REQUEST_READ_FILE = 5;
+    private int PICK_FROM_GALLERY = 1;
+    private static final int PICK_FROM_CAMERA = 2;
+    private SharedPreferences permissionStatus;
+    private boolean sentToSettings = false;
+    private String mCoversationType;
+
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_symptoms);
         setupActionBar();
         mActionBar.setTitle("Symptoms");
+        mActivity = this;
+        permissionStatus = mActivity.getSharedPreferences("permissionStatus", MODE_PRIVATE);
+        mCoversationType = getIntent().getStringExtra("chatType");
+        Button continueButton = (Button) findViewById(R.id.continue_button);
+        continueButton.setOnClickListener(mOnClickListener);
+
+        Button attatchButton = (Button) findViewById(R.id.attach_button);
+        attatchButton.setOnClickListener(mOnClickListener);
+
     }
+
+    private View.OnClickListener mOnClickListener = new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            int id = v.getId();
+            if (id == R.id.continue_button) {
+                if (mCoversationType.equalsIgnoreCase("audio")) {
+                    Intent audioCallIntent = new Intent(SymptomsActivity.this, AudioCallActivityV2.class);
+                    audioCallIntent.putExtra("CONTACT_ID", "reciverUserId");
+                    startActivity(audioCallIntent);
+                    overridePendingTransition(R.anim.slide_in, R.anim.slide_out);
+                } else if (mCoversationType.equalsIgnoreCase("video")) {
+                    Intent videoCallIntent = new Intent(SymptomsActivity.this, VideoActivity.class);
+                    videoCallIntent.putExtra("CONTACT_ID", "reciverUserId");
+                    startActivity(videoCallIntent);
+                    overridePendingTransition(R.anim.slide_in, R.anim.slide_out);
+                } else if (mCoversationType.equalsIgnoreCase("chat")) {
+                    Intent intent = new Intent(SymptomsActivity.this, ConversationActivity.class);
+                    if (ApplozicClient.getInstance(SymptomsActivity.this).isContextBasedChat()) {
+                        intent.putExtra(ConversationUIService.CONTEXT_BASED_CHAT, true);
+                    }
+                    startActivity(intent);
+                    overridePendingTransition(R.anim.slide_in, R.anim.slide_out);
+                } else {
+
+                }
+            } else if (id == R.id.attach_button) {
+                if (!NetworkChangeListener.getNetworkStatus().isConnected()) {
+                    Toast.makeText(mActivity, "No internet connection. Please retry", Toast.LENGTH_SHORT).show();
+                } else {
+                    uploadFile();
+                }
+            }
+
+        }
+    };
 
     @Override
     public void onBackPressed() {
         finish();
         overridePendingTransition(R.anim.slide_in_left, R.anim.slide_out_right);
+    }
+
+    private void uploadFile() {
+        final Dialog dialog = new Dialog(SymptomsActivity.this);
+        dialog.requestWindowFeature(Window.FEATURE_NO_TITLE);
+        dialog.setContentView(R.layout.uploadfile_alertbox);
+        dialog.getWindow().setBackgroundDrawable(new ColorDrawable(android.graphics.Color.TRANSPARENT));
+        dialog.setCancelable(true);
+        dialog.setCanceledOnTouchOutside(true);
+        TextView title = (TextView) dialog.findViewById(R.id.title);
+        TextView item1 = (TextView) dialog.findViewById(R.id.item1_tv);
+        item1.setVisibility(View.GONE);
+        TextView item2 = (TextView) dialog.findViewById(R.id.item2_tv);
+
+        title.setText("Alert");
+        item2.setText("Upload files");
+
+
+        item2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                dialog.dismiss();
+                askRunTimePermissions();
+            }
+        });
+
+        dialog.show();
+    }
+
+    void askRunTimePermissions() {
+
+        if (ActivityCompat.checkSelfPermission(mActivity, permissionsRequired[0]) != PackageManager.PERMISSION_GRANTED
+                || ActivityCompat.checkSelfPermission(mActivity, permissionsRequired[1]) != PackageManager.PERMISSION_GRANTED) {
+            if (ActivityCompat.shouldShowRequestPermissionRationale(mActivity, permissionsRequired[0]) || ActivityCompat.shouldShowRequestPermissionRationale(mActivity, permissionsRequired[1])) {
+                //Show Information about why you need the permission
+                android.support.v7.app.AlertDialog.Builder builder = new android.support.v7.app.AlertDialog.Builder(mActivity);
+                builder.setTitle("Need multiple permissions");
+                builder.setMessage("This app needs camera and storage permissions.");
+                builder.setPositiveButton("Grant", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        dialog.cancel();
+                        ActivityCompat.requestPermissions(mActivity, permissionsRequired, PERMISSION_CALLBACK_CONSTANT);
+                    }
+                });
+                builder.setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        dialog.cancel();
+                    }
+                });
+                builder.show();
+            } else if (permissionStatus.getBoolean(permissionsRequired[0], false)) {
+                //Previously Permission Request was cancelled with 'Dont Ask Again',
+                // Redirect to Settings after showing Information about why you need the permission
+                android.support.v7.app.AlertDialog.Builder builder = new android.support.v7.app.AlertDialog.Builder(mActivity);
+                builder.setTitle("Need multiple permissions");
+                builder.setMessage("This app needs camera and storage permissions.");
+                builder.setPositiveButton("Grant", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        dialog.cancel();
+                        sentToSettings = true;
+                        Intent intent = new Intent(Settings.ACTION_APPLICATION_DETAILS_SETTINGS);
+                        Uri uri = Uri.fromParts("package", mActivity.getPackageName(), null);
+                        intent.setData(uri);
+                        startActivityForResult(intent, REQUEST_PERMISSION_SETTING);
+                        Toast.makeText(mActivity.getBaseContext(), "Go to permissions to grant  camera and storage", Toast.LENGTH_LONG).show();
+                    }
+                });
+                builder.setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        dialog.cancel();
+                    }
+                });
+                builder.show();
+            } else {
+                //just request the permission
+                ActivityCompat.requestPermissions(mActivity, permissionsRequired, PERMISSION_CALLBACK_CONSTANT);
+            }
+
+            SharedPreferences.Editor editor = permissionStatus.edit();
+            editor.putBoolean(permissionsRequired[0], true);
+            editor.commit();
+        } else {
+            //You already have the permission, just go ahead.
+            proceedAfterPermission();
+        }
+
+
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        if (requestCode == PERMISSION_CALLBACK_CONSTANT) {
+            //check if all permissions are granted
+            boolean allgranted = false;
+            for (int i = 0; i < grantResults.length; i++) {
+                if (grantResults[i] == PackageManager.PERMISSION_GRANTED) {
+                    allgranted = true;
+                } else {
+                    allgranted = false;
+                    break;
+                }
+            }
+            if (allgranted) {
+                proceedAfterPermission();
+            } else if (ActivityCompat.shouldShowRequestPermissionRationale(mActivity, permissionsRequired[0])
+                    || ActivityCompat.shouldShowRequestPermissionRationale(mActivity, permissionsRequired[1])) {
+                android.support.v7.app.AlertDialog.Builder builder = new android.support.v7.app.AlertDialog.Builder(mActivity);
+                builder.setTitle("Need multiple permissions");
+                builder.setMessage("This app needs camera and storage permissions.");
+                builder.setPositiveButton("Grant", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        dialog.cancel();
+                        ActivityCompat.requestPermissions(mActivity, permissionsRequired, PERMISSION_CALLBACK_CONSTANT);
+                    }
+                });
+                builder.setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        dialog.cancel();
+                    }
+                });
+                builder.show();
+            } else {
+                Toast.makeText(mActivity.getBaseContext(), "Unable to get Permission", Toast.LENGTH_LONG).show();
+            }
+        } else if (requestCode == MY_PERMISSIONS_REQUEST_READ_FILE) {
+            if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+            } else {
+            }
+        }
+    }
+
+
+    private void proceedAfterPermission() {
+        //chooseimage();
     }
 }
