b1038fc2d71c00876a1b5d81257e0b4996743ade
ayazAndroid <ayazhealthscion.com>
2017-06-23 15:57:26 +0530

    Fixed Issue

---
+++ app/src/main/java/ui/PastVisitedPatientDetail.java
@@ -0,0 +1,18 @@
+package ui;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+
+import com.hs.userportal.R;
+
+/**
+ * Created by ayaz on 23/6/17.
+ */
+
+public class PastVisitedPatientDetail extends BaseActivity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_past_visit);
+    }
+}
