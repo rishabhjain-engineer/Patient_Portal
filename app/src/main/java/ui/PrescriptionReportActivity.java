ff763725e4193c7af9840b2fdfba428e5a498c21
ayazAndroid <ayazhealthscion.com>
2017-06-28 14:19:14 +0530

    Fixed Issues

--- app/src/main/java/ui/PrescriptionReportActivity.java
+++ app/src/main/java/ui/PrescriptionReportActivity.java
@@ -1,8 +1,17 @@
 package ui;
 
+import android.app.AlertDialog;
 import android.app.ProgressDialog;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.net.Uri;
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.os.Environment;
+import android.os.StrictMode;
 import android.support.annotation.Nullable;
+import android.util.Log;
 import android.widget.BaseAdapter;
 import android.widget.Toast;
 
@@ -11,15 +20,34 @@ import com.android.volley.Response;
 import com.android.volley.VolleyError;
 import com.android.volley.toolbox.JsonObjectRequest;
 import com.android.volley.toolbox.Volley;
+import com.hs.userportal.ReportStatus;
 
+import org.apache.http.client.ClientProtocolException;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.entity.StringEntity;
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
+
+import java.io.BufferedReader;
+import java.io.ByteArrayInputStream;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.io.UnsupportedEncodingException;
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+import java.util.List;
 
 import config.StaticHolder;
 import models.PastVisitDoctorListModel;
 import networkmngr.NetworkChangeListener;
 import utils.AppConstant;
+import utils.PreferenceHelper;
 
 /**
  * Created by ayaz on 24/6/17.
@@ -37,20 +65,22 @@ public class PrescriptionReportActivity extends BaseActivity {
         mProgressDialog.setCancelable(false);
         mProgressDialog.setMessage("Loading...");
         mProgressDialog.setIndeterminate(true);
+        Intent intent = getIntent();
+        String consultId = intent.getStringExtra("temporary");
         if (NetworkChangeListener.getNetworkStatus().isConnected()) {
             mProgressDialog.show();
-            getPrescriptionReport();
+            getPrescriptionReport(consultId);
         } else {
             Toast.makeText(PrescriptionReportActivity.this, "No internet connection. Please retry.", Toast.LENGTH_SHORT).show();
         }
     }
 
-    private void getPrescriptionReport() {
+    private void getPrescriptionReport(String consultId) {
         StaticHolder static_holder = new StaticHolder(this, StaticHolder.Services_static.GetPrescriptionReport);
         String url = static_holder.request_Url();
         JSONObject data = new JSONObject();
         try {
-            data.put("consultId", AppConstant.getPatientID());
+            data.put("consultId", consultId);
         } catch (JSONException je) {
             je.printStackTrace();
         }
@@ -86,4 +116,212 @@ public class PrescriptionReportActivity extends BaseActivity {
         });
         mRequestQueue.add(symptomsJsonObjectRequest);
     }
+
+    private String ptname;
+    private byte[] result = null;
+    class pdfprocess extends AsyncTask<Void, String, Void> {
+        @Override
+        protected void onPreExecute() {
+            super.onPreExecute();
+            ptname = mPreferenceHelper.getString(PreferenceHelper.PreferenceKey.USER_NAME);
+            ptname.replaceAll(" ", "_");
+            mProgressDialog.show();
+        }
+
+        @Override
+        protected Void doInBackground(Void... params) {
+            int count;
+            File reportFile = null;
+            File sdCard = Environment.getExternalStorageDirectory();
+            File dir = new File(sdCard.getAbsolutePath() + "/Lab Pdf/");
+            if (!dir.exists()) {
+                dir.mkdirs();
+            }
+
+            JSONObject dataToSend = new JSONObject();
+            try {
+                dataToSend.put("consultId", consultId);
+            } catch (JSONException je) {
+                je.printStackTrace();
+            }
+
+            reportFile = new File(dir.getAbsolutePath(), ptname + "report.pdf");
+            result = service.pdf(dataToSend, "Report Status");
+            int lenghtOfFile = 1;
+            if (result != null) {
+                lenghtOfFile = result.length;
+                String temp = null;
+                try {
+                    if(result != null && result.length >0){
+                        temp = new String(result, "UTF-8");
+                    }
+                } catch (UnsupportedEncodingException e1) {
+                    e1.printStackTrace();
+                }
+                OutputStream out;
+                try {
+                    out = new FileOutputStream(reportFile);
+                    InputStream input = new ByteArrayInputStream(result);
+                    out = new FileOutputStream(reportFile);
+
+                    byte data[] = new byte[1024];
+
+                    long total = 15;
+
+                    while ((count = input.read(data)) != -1) {
+                        total += count;
+                        publishProgress("" + (int) ((total * 100) / lenghtOfFile));
+                        out.write(result);
+                    }
+                    out.flush();
+                    out.close();
+                } catch (FileNotFoundException e) {
+                    e.printStackTrace();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+            return null;
+
+        }
+
+        @Override
+        protected void onPostExecute(Void aaa) {
+            super.onPostExecute(aaa);
+            if(mProgressDialog != null && mProgressDialog.isShowing()){
+                mProgressDialog.dismiss();
+            }
+            if(result != null){
+                try {
+
+                    File sdCard = Environment.getExternalStorageDirectory();
+                    File dir = new File(sdCard.getAbsolutePath() + "/Lab Pdf/");
+                    File fileReport = new File(dir.getAbsolutePath(), ptname + "report.pdf");
+
+                    PackageManager packageManager = getPackageManager();
+                    Intent intent = new Intent(Intent.ACTION_VIEW);
+                    intent.setType("application/pdf");
+
+                    @SuppressWarnings("rawtypes")
+                    List list = packageManager.queryIntentActivities(intent,
+                            PackageManager.MATCH_DEFAULT_ONLY);
+
+                    if (list.size() > 0 && fileReport.isFile()) {
+                        Log.v("post", "execute");
+
+                        Intent objIntent = new Intent(Intent.ACTION_VIEW);
+                        ///////
+                        Uri uri = null;
+                        //if (Build.VERSION.SDK_INT < Build.VERSION_CODES.N) {
+                        Method m = null;
+                        try {
+                            m = StrictMode.class.getMethod("disableDeathOnFileUriExposure");
+                            m.invoke(null);
+                        } catch (NoSuchMethodException e) {
+                            e.printStackTrace();
+                        } catch (IllegalAccessException e) {
+                            e.printStackTrace();
+                        } catch (InvocationTargetException e) {
+                            e.printStackTrace();
+                        }
+                        uri = Uri.fromFile(fileReport);
+                    /*} else {
+                        uri = FileProvider.getUriForFile(ReportRecords.this, getApplicationContext().getPackageName() + ".provider", fileReport);
+                    }*/
+                        objIntent.setDataAndType(uri, "application/pdf");
+                        objIntent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+                        startActivity(objIntent);//Staring the pdf viewer
+                    } else if (!fileReport.isFile()) {
+                        Log.v("ERROR!!!!", "OOPS2");
+                    } else if (list.size() <= 0) {
+                        AlertDialog.Builder dialog = new AlertDialog.Builder(PrescriptionReportActivity.this);
+                        dialog.setTitle("PDF Reader not found");
+                        dialog.setMessage("A PDF Reader was not found on your device. The Report is saved at "
+                                + fileReport.getAbsolutePath());
+                        dialog.setCancelable(false);
+                        dialog.setPositiveButton("OK",
+                                new DialogInterface.OnClickListener() {
+
+                                    @Override
+                                    public void onClick(DialogInterface dialog,
+                                                        int which) {
+                                        // TODO Auto-generated method stub
+                                        dialog.dismiss();
+                                    }
+                                });
+                        dialog.show();
+                    }
+
+                } catch (OutOfMemoryError e) {
+                    AlertDialog.Builder dlg = new AlertDialog.Builder(PrescriptionReportActivity.this);
+                    dlg.setTitle("Not enough memory");
+                    dlg.setMessage("There is not enough memory on this device.");
+                    dlg.setPositiveButton("Ok", new DialogInterface.OnClickListener() {
+                                @Override
+                                public void onClick(DialogInterface dialog, int which) {
+                                    PrescriptionReportActivity.this.finish();
+                                }
+                            });
+                    e.printStackTrace();
+                }
+            }else{
+                Toast.makeText(PrescriptionReportActivity.this, "An error occured, Please try after some time.", Toast.LENGTH_SHORT).show();
+            }
+        }
+    }
+
+
+    /*public byte[] pdf(JSONObject sendData, String actName) {
+        byte[] fileContents = null;
+        StaticHolder sttc_holdr = new StaticHolder(StaticHolder.Services_static.GetpatienttestReportAndroid);
+        String url = sttc_holdr.request_Url();
+        //String url = "https://patient.cloudchowk.com:8081/WebServices/HTMLReports.asmx/GetpatienttestReportHTMLAndroid";
+        request = new HttpPost(url);
+        request.setHeader("Content-type", "application/json");
+        request.setHeader("Accept", "application/octet-stream");
+        request.setHeader("Cookie", hoja);
+        String cookieData = "";
+
+        for (int i = 0; i < cookies.length; i++) {
+            cookieData += cookies[i].getValue() + ";";
+        }
+
+        request.addHeader("Cookie", cookieData);
+
+        try {
+            request.setEntity(new StringEntity(sendData.toString(), "UTF-8"));
+            response = client.execute(request);
+
+            reader = new BufferedReader(new InputStreamReader(response.getEntity().getContent()));
+            sb = new StringBuilder();
+            while ((line = reader.readLine()) != null) {
+                sb.append(line + "\n");
+            }
+
+            receiveData = new JSONObject(new String(sb));
+            Log.i("REPORT DETAILS", receiveData.toString());
+
+            String p = receiveData.get("d").toString();
+            String[] byteValues = p.substring(1, p.length() - 1).split(",");
+            byte[] bytes = new byte[byteValues.length];
+            for (int i = 0, len = bytes.length; i < len; i++) {
+                bytes[i] = (byte) Integer.valueOf(byteValues[i].trim()).byteValue();
+            }
+            fileContents = bytes;
+        } catch (UnsupportedEncodingException e) {
+            e.printStackTrace();
+        } catch (ClientProtocolException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        } catch (JSONException e) {
+            e.printStackTrace();
+        } catch (NumberFormatException ex) {
+            if (ReportStatus.progress != null) {
+                ReportStatus.progress.dismiss();
+            }
+            ReportStatus.progress = null;
+        }
+        return fileContents;
+    }*/
 }
